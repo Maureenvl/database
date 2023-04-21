9e51ab74fa3b1d64237293de3cdf2d6ee669b1c3
Maureen <220000@student.glu.nl>
2023-04-21 11:16:56 +0200

    pup

--- week2/index.php
+++ week2/index.php
@@ -1,14 +1,25 @@
 <?php
 $servername = "localhost";
-$username = "";
-$password = "";
+$username = "root";
+$password = "root";
+$dbname = "Fictief";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+$sql = "SELECT * FROM `producten` WHERE 1";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+foreach($stmt->fetchAll() as $k=>$v) {
+  echo $v['Naam'];
+}
+
 ?> 
